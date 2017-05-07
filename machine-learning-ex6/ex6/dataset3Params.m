361dde6353eba2791253688ce161b61cf6b23899
anthonyomorada <anthony.omorada@gmail.com>
2017-05-07 12:22:00 -0700

    ex-6-solved

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,6 +23,22 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+test = [0.01 0.03 0.1 0.3 1 3 10 30];
+
+for i=1:length(test)
+    for j=1:length(test)
+            model = svmTrain(X, y, test(i), @(x1, x2) gaussianKernel(x1, x2, test(j)));
+            predict = svmPredict(model, Xval);
+            crossval_errors(i,j) = mean(double(predict ~= yval));
+    end;
+end
+
+[MinColumn, MinColumnIndex] = min(crossval_errors);
+[MinValue, MinValueIndex] = min(MinColumn);
+
+C = test(MinColumnIndex(MinValueIndex));
+sigma = test(MinValueIndex);
+
 
 
 
